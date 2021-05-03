10c650d9df38087ab693d2e2d5643c9e81609d66
Agata Liberska <liberska@gmail.com>
2021-05-03 11:26:09 +0100

    add info section

--- src/components/ButtonElement.js
+++ src/components/ButtonElement.js
@@ -3,11 +3,11 @@ import {Link} from 'react-scroll';
 
 export const Button = styled(Link)`
     border-radius: 50px;
-    background: ${({primary}) => primary ? '#01Bf71' : '#010606'};
+    background: ${({primary}) => (primary ? '#01Bf71' : '#010606')};
     white-space: nowrap;
-    padding: ${({big}) => big ? '14px 48px' : '12px 30px'};
-    color: ${({dark}) => dark ? '#010606' : '#fff'};
-    font-size: ${({fontBig}) => fontBig ? '20px' : '16px'};
+    padding: ${({big}) => (big ? '14px 48px' : '12px 30px')};
+    color: ${({dark}) => (dark ? '#010606' : '#fff')};
+    font-size: ${({fontBig}) => (fontBig ? '20px' : '16px')};
     border: none;
     cursor: pointer;
     display: flex;
@@ -16,6 +16,6 @@ export const Button = styled(Link)`
     transition: all 0.2s ease-in-out;
 
     &:hover {
-        background: ${({primary}) => primary ?'#fff' : '#01Bf71'};
+        background: ${({primary}) => (primary ?'#fff' : '#01Bf71')};
     }
 `
