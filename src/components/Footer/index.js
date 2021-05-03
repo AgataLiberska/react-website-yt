aa6e9b8b5cf3c6ecce0b4cae9d6d73444958f2ed
Agata Liberska <liberska@gmail.com>
2021-05-03 14:56:30 +0100

    add social media links to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaFacebook, FaInstagram, FaYoutube, FaTwitter, FaLinkedin } from 'react-icons/fa';
 import {
     FooterContainer,
     FooterWrap, 
@@ -6,7 +7,13 @@ import {
     FooterLinksWrapper,
     FooterLinkItems,
     FooterLinkTitle,
-    FooterLink
+    FooterLink,
+    SocialMedia,
+    SocialMediaWrap,
+    SocialLogo, 
+    WebsiteRights,
+    SocialIcons,
+    SocialIconLink
 } from './FooterElements';
 
 const Footer = () => {
@@ -56,6 +63,31 @@ const Footer = () => {
                         </FooterLinkItems>
                     </FooterLinksWrapper>
                 </FooterLinksContainer>
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to='/'>
+                            dolla
+                        </SocialLogo>
+                        <WebsiteRights>dolla &copy; {new Date().getFullYear()} All Rights Reserved</WebsiteRights>
+                        <SocialIcons>
+                            <SocialIconLink href='/' target='_blank' aria-label='Facebook'>
+                                <FaFacebook />
+                            </SocialIconLink>
+                            <SocialIconLink href='/' target='_blank' aria-label='Instagram'>
+                                <FaInstagram />
+                            </SocialIconLink>
+                            <SocialIconLink href='/' target='_blank' aria-label='YouTube'>
+                                <FaYoutube />
+                            </SocialIconLink>
+                            <SocialIconLink href='/' target='_blank' aria-label='Twitter'>
+                                <FaTwitter />
+                            </SocialIconLink>
+                            <SocialIconLink href='/' target='_blank' aria-label='LinkedIn'>
+                                <FaLinkedin />
+                            </SocialIconLink>
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                </SocialMedia>
             </FooterWrap>
         </FooterContainer>
     )
